2914bf8381b4537a6364688a6fcebd9080994b1b
HGCollier <haldengcollier@gmail.com>
2025-04-03 22:22:30 +0100

    Created alert component.

---
+++ src/Waizor/Components/Alert/Alert.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Waizor.Components;
+
+public partial class Alert : ComponentBase
+{
+    [Parameter]
+    public required RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+}
