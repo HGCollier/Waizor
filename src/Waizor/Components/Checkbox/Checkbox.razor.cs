18d3b8eb83ebffe682135c964b751c7bf3884ac8
HGCollier <haldengcollier@gmail.com>
2025-04-07 17:52:09 +0100

    Created `Checkbox` component.

---
+++ src/Waizor/Components/Checkbox/Checkbox.razor.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Waizor.Components;
+
+public partial class Checkbox<T> : ComponentBase
+{
+    [Parameter]
+    public required RenderFragment ChildContent { get; set; }
+
+    [Parameter]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+
+    [Parameter]
+    public string? Name { get; set; }
+
+    [Parameter]
+    public string? Class { get; set; }
+
+    [Parameter]
+    public bool Checked { get; set; }
+
+    [Parameter]
+    public EventCallback<bool> CheckedChanged { get; set; }
+
+    [Parameter]
+    public T? Value { get; set; }
+
+    [Parameter]
+    public bool Disabled { get; set; }
+
+    [Parameter]
+    public bool ReadOnly { get; set; }
+
+    [Parameter]
+    public bool Required { get; set; }
+
+    private async Task OnClickAsync()
+    {
+        if (ReadOnly)
+        {
+            return;
+        }
+
+        Checked = !Checked;
+
+        await CheckedChanged.InvokeAsync(Checked);
+    }
+}
