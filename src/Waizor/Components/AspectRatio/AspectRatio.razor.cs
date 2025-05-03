e51c868610dadf930434f3d44f712097a9d5f94d
Halden Collier <haldengcollier@gmail.com>
2025-05-03 16:52:20 +0100

    Created an `AspectRatio` component.

---
+++ src/Waizor/Components/AspectRatio/AspectRatio.razor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace Waizor.Components
+{
+    public sealed partial class AspectRatio : ComponentBase
+    {
+        [Parameter, EditorRequired]
+        public required RenderFragment ChildContent { get; set; }
+
+        [Parameter, EditorRequired]
+        public double Ratio { get; set; }
+    }
+}
